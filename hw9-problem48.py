94af501114d986fbec62fdfd1c605cf3d28e74ef
hrz <bagzina@gmail.com>
2021-08-02 12:08:43 +0300

    add hw 9_problem48

---
+++ hw9-problem48.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# problem48 - list comprehension : one line
+#
+# The series, 1**1 + 2**2 + 3**3 + ... + 10**10 = 10405071317.
+#
+# Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000.
+
+result = [sum(x**x for x in range(1, 1001))]
+print(str(result[0])[-10:])
