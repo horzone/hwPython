3444057196908644ee108f92c1a7c1578bf5c4c9
hrz <bagzina@gmail.com>
2021-08-02 14:53:47 +0300

    hw4 after tips

--- hw4.py
+++ hw4.py
@@ -3,16 +3,18 @@
 
 def function(my_input):
     input_line = []
-    for i in my_input:
-        if i.isnumeric():
-            input_line.append(i)
+    for i in range(0, len(my_input)):
+        if my_input[i].isnumeric():
+            input_line.append(my_input[i])
+        elif my_input[i] == "-" and my_input[i+1].isnumeric():
+            input_line.append(my_input[i])
         else:
             input_line.append(" ")
     input_line = "".join(input_line)
     input_line = input_line.split(sep=" ")
     result = 0
     for j in input_line:
-        if j.isnumeric():
+        if j != "":
             result += int(j)
     print(result)
 
