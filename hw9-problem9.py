aaafc884eb16a293a9d1206fc9a3d9f54174e3b9
hrz <bagzina@gmail.com>
2021-08-03 15:08:33 +0300

    fix hw9-problem9

--- hw9-problem9.py
+++ hw9-problem9.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from functools import reduce
 
 # problem9 - list comprehension : one line
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
@@ -10,11 +10,8 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-for c in range(1, 1000):
-    for b in range(1, c):
-        for a in range(1, b):
-            if a ** 2 + b ** 2 == c ** 2:
-                if a + b + c == 1000:
-                    print("{} {} {}".format(a, b, c))
 
+result = [c*b*a for c in range(1, 1001) for b in range(1, c) for a in range(1, b) if
+                                     a ** 2 + b ** 2 == c ** 2 and a + b + c == 1000]
 
+print(result)
