8278cdd8109bb863676e8f5345517af0aca9ddcd
hrz <bagzina@gmail.com>
2021-08-03 15:45:08 +0300

    add hw14

--- hw14.py
+++ hw14.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
+import pickle
+
 
 # Создать сотрудника Mary, пользуясь классом
 # Employee и перенести его в другую программу,
 # используя модуль Pickle и файловую систему.
 # Узнать про + и - модуля Pickle.
+
+#TODO Можно спрятать в битовой последовательности любую функцию, и при импорте она сработает (возможна вреданосная)
+
+
+
+
+
+
+class Employee:
+    def __init__(self, name):
+        self.name = name
+
+
+mary = Employee('Mary')
+
+with open('HW14export', 'wb') as export_file:
+    pickle.dump(mary, export_file)
 
