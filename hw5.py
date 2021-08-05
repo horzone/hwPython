7a1fc317baeed681b71c4bc35ce8920569d58ea5
hrz <bagzina@gmail.com>
2021-08-05 11:32:40 +0300

    fix hw5

--- hw5.py
+++ hw5.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 
+# 1. После запуска предлагает пользователю ввести неотрицательные целые числа,
+# разделенные через пробел и ожидает ввода от пользователя.
+# 2. Находит наименьшее положительное число, не входящее в данный пользователем
+# список чисел и печатает его.
 
 def function(my_input):
     my_input = my_input.split(sep=" ")
     my_input = [int(i) for i in my_input]
-    result = 1
-    for i in set(my_input):
-        if result in my_input:
-            result += 1
-        else:
-            print(result)
-            exit()
+    for i in range(1, max(my_input)+1):
+        if i not in my_input:
+            print(i)
+            return
+        elif i == (max(my_input)):
+            print(i+1)
 
 
 print("Введите неотрицательные целые числа, разделенные через пробел: ")
