1e69d681cd386a93fda6185af066e5f08342f894
hrz <bagzina@gmail.com>
2021-08-09 19:42:32 +0300

    fix hw16

--- hw16.py
+++ hw16.py
@@ -10,16 +10,18 @@ import googlemaps
 # Location: Теремок, Енотаевская улица, Удельная, округ Светлановское, Выборгский район, Санкт-Петербург, Северо-Западный федеральный округ, 194017, РФ
 # Goggle Maps URL: https://www.google.com/maps/search/?api=1&query=60.016666666666666,30.322
 
+def function(latlng):
+    me = googlemaps.Client(key='AIzaSyCrEsxkaBbNDuk5agv0boIj6QttGxv7tRs')
+    request = googlemaps.client.reverse_geocode(latlng=latlng, client=me, language='ru')
+    place_id = request[0]['place_id']
+    place_address = request[0]['formatted_address']
+    place_request = googlemaps.client.place(place_id=place_id, fields=["name", "url"], language='ru',
+                                            client=me)
+    place_name = place_request['result']['name']
+    place_url = place_request['result']['url']
+    print(f'Location: {place_name}, {place_address}')
+    print(f'Google Maps URL: {place_url}')
 
 with open('gps.txt', 'r') as my_file:
     for line in my_file:
-        me = googlemaps.Client(key='AIzaSyCrEsxkaBbNDuk5agv0boIj6QttGxv7tRs')
-        request = googlemaps.client.reverse_geocode(latlng=line, client=me, language='ru')
-        place_id = request[0]['place_id']
-        place_address = request[0]['formatted_address']
-        place_request = googlemaps.client.place(place_id=place_id, fields=["name", "url"], language='ru',
-                                                client=me)
-        place_name = place_request['result']['name']
-        place_url = place_request['result']['url']
-        print(f'Location: {place_name}, {place_address}')
-        print(f'Google Maps URL: {place_url}')
+        function(latlng=line)
