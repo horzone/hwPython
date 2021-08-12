52c162c559a68fdee8a3c62f959548f6fe781d18
hrz <bagzina@gmail.com>
2021-08-12 09:40:22 +0300

    fix hw19

--- hw19_client.py
+++ hw19_client.py
@@ -5,7 +5,7 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
     # Просим ввессти запрос
-    message = input("Input domain name: ")
+    message = input("Input domain name(*shutdown* for exit and shutdown server: ")
     # Отправляем наш запрос на сервер
     client.sendto(bytes(message, 'utf-8'), ('127.0.0.1', 4041))
     # Если запрос на выключение DNS сервера, то выходим из цикла
