ba2dce042fa29e8b21b1ed1be180589e53c91749
hrz <bagzina@gmail.com>
2021-08-17 15:25:13 +0300

    fix hw23

--- hw23.py
+++ hw23.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import copy
 # Надо написать функцию которая возвращает N-мерный массив с ширинами заданными в аргументе списком из N элементов:
 # n_arr([2,2])
 # >> [[“”,“”],[“”,“”]]
@@ -7,22 +8,19 @@
 
 
 def n_arr(size):
-    element = "!"
+    element = ('!')
     no_result = []
     for _ in range(0, size[0]):
         no_result.append(element)
-    next_count = list(no_result)
+    next_count = copy.deepcopy(no_result)
     for i in range(1, len(size)):
-        prev_count = list([next_count])
-        for j in range(0,size[i]-1):
-            prev_count.append(list(next_count))
-            if size[i] - (j+2) == 0:
-                next_count = list(prev_count)
-    print(next_count)
+        prev_count = [copy.deepcopy(next_count)]
+        for j in range(0, size[i] - 1):
+            prev_count.append(copy.deepcopy(next_count))
+            if size[i] - (j + 2) == 0:
+                next_count = copy.deepcopy(prev_count)
+    return next_count
 
 
-n_arr([2, 2])
-n_arr([2, 2, 2])
-n_arr([2, 2, 2, 2])
-n_arr([2, 2, 2, 2, 2])
-n_arr([2, 2, 2, 2, 2, 2])
+a = n_arr([1, 3, 3])
+print(a)
